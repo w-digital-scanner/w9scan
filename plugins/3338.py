ce59e987e08be3dfd7e6135dc32a77a92b14cf19
boy-hack <34109680@qq.com>
2017-12-22 15:51:16 +0800

    载入ip扫描的插件

--- plugins/3338.py
+++ plugins/3338.py
@@ -1,2 +1,15 @@
 #!/usr/bin/evn python
+import socket
+import urlparse
 
+def assign(service, arg):
+    if service == 'www':
+        url_info = urlparse.urlparse(arg)
+        hostname = socket.gethostbyname(url_info.netloc)
+        return True, hostname
+
+def audit(arg):
+    task_push("ip",arg)
+
+if __name__ == "__main__":
+    print assign("www","https://blog.hacking8.com")
