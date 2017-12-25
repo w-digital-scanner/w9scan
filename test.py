280cf4696c3499ca3881d3b1ee94d11c66e0095c
boy-hack <34109680@qq.com>
2017-12-26 00:11:06 +0800

    改进插件加载机制

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-from thirdparty import hackhttp
-hh = hackhttp.hackhttp()
-url = "http://www.adfun.cn/"
-code, head, html, redirect_url, log = hh.http(url) #code, head, html, redirect_url, log = hh.http(url)
-print code, head, html, redirect_url, log
+import os
+foder = r'C:\Users\boyhack\Desktop\w9scan\plugins'
+
+filter_func = lambda file: (True, False)['__init__' in file or 'pyc' in file]
+def getExp():
+    direxp = []
+    for dirpath, dirnames, filenames in os.walk(foder):
+        for filename in filenames:
+            direxp.append(os.path.join(dirpath,filename))
+    return direxp
+for exp in getExp():
+    print exp
+#dir_exploit = filter(filter_func,getExp())
+#print dir_exploit
