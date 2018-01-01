e01b8e3f5071e060468dfe50a42462c4c4d7443b
boy-hack <34109680@qq.com>
2018-01-01 20:28:07 +0800

    change version to 1.4.1

---
+++ plugins/spider_file/Collect_info.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#__author__ = '1c3z'
+
+import re
+
+def assign(service, arg):
+    if service == 'spider_file':
+        return True, arg
+
+def fetch(url,raw):
+    card15 = r"\b([1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3})\b"
+    card18 = r"\b([1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X))\b"
+    URL = r"\b(http://(\d{1,3}\.){3}\d{1,3}(:\d+)?)\b"
+    mail = r"\b([a-zA-Z\-_1-9]+@([a-zA-Z\-_1-9]+\.){1,3}(com|cn|net|org))\b"
+    phone = r"\b((13|14|15|17|18)\d{9})\b"
+
+    regexs = [card15,card18,mail,phone,URL]
+
+    for r in regexs:
+        rst = re.findall(r, raw)
+        for i in rst:
+            security_note("Infomation Collect:" + i[0])
+
+def audit(url,html):
+    fetch(url,html)
---
+++ plugins/spider_file/Xss_scan.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import urlparse
+from urllib import quote as urlencode
+
+def assign(service, arg):
+    if service == 'spider_file':
+        return True, arg
+
+def audit(url,html):
+    parse = urlparse.urlparse(url)
+    if not parse.query:
+        return
+    
+    for path in parse.query.split('&'):
+        k, v = path.split('=')
+        XSS_PAYLOAD	= [
+			'<script>alert(1);</script>',
+			'<script>prompt(1);</script>',
+			'<script>confirm(1);</script>',
+			'<scr<script>ipt>alert(1)</scr<script>ipt>',
+			'<object data="data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTs8L3NjcmlwdD4=">',
+			'<svg/onload=prompt(1);>',
+			'<marquee/onstart=confirm(1)>/',
+			'<body onload=prompt(1);>',
+			'<select autofocus onfocus=alert(1)>',
+			'<textarea autofocus onfocus=alert(1)>',
+			'<keygen autofocus onfocus=alert(1)>',
+			'<video><source onerror="javascript:alert(1)">'		
+	    ]
+        for payload in XSS_PAYLOAD:
+            url_1 = url.replace("%s=%s"%(k,v),"%s=%s"%(k,urlencode(payload)))
+            code, head, html, redirect_url, log = hackhttp.http(url_1)
+            if payload in html:
+                security_hole("[XSS] " + url_1)
