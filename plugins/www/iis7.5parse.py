9d5515a60bbd6b9b332460087faaa294e0181aa8
boy-hack <34109680@qq.com>
2018-02-08 17:20:45 +0800

    add iis7.5 parse

---
+++ plugins/www/iis7.5parse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import re
+import urlparse
+
+
+def assign(service, arg):
+    if service == "www":
+        return True, arg
+
+
+def audit(arg):
+    path = "/robots.txt/.php"
+    code, head, res, errcode, _ = curl.curl(arg + x)
+    if code == 200 and "User-agent" in res:
+        security_note("存在解析漏洞:" + arg + x)
+
+
+if __name__ == '__main__':
+    from dummy import *
+
+    audit(assign('www', 'http://blog.hacking8.com/')[1])
