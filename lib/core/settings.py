8b4952ce578c814dd595270bc3513b354b7c5374
boy-hack <34109680@qq.com>
2018-02-26 19:24:00 +0800

    修改banner

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -2,23 +2,20 @@
 
 # w9scan version
 VERSION = "1.8.3"
-Site = "https://blog.hacking8.com/"
+Site = "https://github.com/boy-hack/w9scan"
+AUTHOR = "w8ay"
+MAIL = "master@hacking8.com"
 
 # w9scan banner
-banner = """
-        (()___(()
-        /        \\
-       ( /     \  \       W9scan v%s is running!
-        \ o  o    /    
-        (_()__)__/ \      Author:w8ay      
-       / __,==.____ \   
-      (    |--|      )    Email: w8ay@qq.com
-      /\_..|__|'-.__/\_
-     / (         /     \  Blog: %s
-     \  \       (      /
-      )  '.______)    /  
-   (((____.---(((____/
-"""%(VERSION,Site)
+banner = """\033[01;34m
+           ____                       \033[01;31m__/\033[01;34m
+ _      __/ __ \______________ _____ \033[01;33m/ \033[01;31m__/\033[01;34m
+| | /| / / /_/ / ___/ ___/ __ `/ __ \\ \033[01;33m_/\033[01;34m
+| |/ |/ /\__, (__  ) /__/ /_/ / / / /
+|__/|__//____/____/\___/\__,_/_/ /_/
+
+\033[01;37m{\033[01;m Version %s by %s mail:%s \033[01;37m}\033[0m
+\n""" % (VERSION, AUTHOR, MAIL)
 
 
 # Format used for representing invalid unicode characters
