10d155423be3289c50c00d0b2539757d52524d4d
boy-hack <34109680@qq.com>
2018-02-27 15:58:20 +0800

    优化异常状态管理

--- lib/core/exception.py
+++ lib/core/exception.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class ReadPluginsException(Exception):
-    pass
-
-class LoadPluginException(Exception):
-    pass
-
-class LoadModuleException(Exception):
-    pass
-
-class BuildHtmlErrorException(Exception):
-    pass
-
-class SaveReportException(Exception):
-    pass
-
 class ToolkitBaseException(Exception):
     pass
 
