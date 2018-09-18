a42a701dddb02a413f167f816b56a2fe3232630f
rootkit <1576235794@qq.com>
2018-09-18 18:19:15 +0800

    删除重复插件

--- plugins/dedecms/67.py
+++ plugins/dedecms/67.py
@@ -9,7 +9,7 @@ def assign(service, arg):
 def audit(arg):
     url = arg + 'data/mysql_error_trace.inc'
     _, _, body, _, _ = curl.curl(url)
-    if body and body.find('<?php  exit();') != -1:
+    if body and '<?php  exit();' in body:
         security_note(url)
 
 if __name__ == '__main__':
