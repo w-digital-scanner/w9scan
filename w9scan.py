253ee03c9c33ac668546620d3a97a62a39dd5b6b
boy-hack <34109680@qq.com>
2018-09-19 10:53:32 +0800

    typo for w9scan.py

--- w9scan.py
+++ w9scan.py
@@ -5,32 +5,29 @@
 
 import argparse
 import inspect
-import multiprocessing
 import os
 import sys
-import time
 from distutils.version import LooseVersion
 
-from lib.core.common import (Banner, createIssueForBlog, getUnicode, makeurl,
-                             printMessage, setPaths, systemQuit, weAreFrozen)
-from lib.core.data import logger, urlconfig,paths
+from lib.core.common import (
+    Banner, createIssueForBlog, getUnicode, setPaths, systemQuit, weAreFrozen)
+from lib.core.data import logger, paths
 from lib.core.engine import pluginScan, webScan
 from lib.core.enums import EXIT_STATUS
 from lib.core.exception import (ToolkitMissingPrivileges,
                                 ToolkitPluginException, ToolkitSystemException,
                                 ToolkitUserQuitException)
-from lib.core.exploit import Exploit_run
 from lib.core.option import initOption
-from lib.core.settings import IS_WIN, LIST_PLUGINS, VERSION
+from lib.core.settings import IS_WIN, VERSION
 from lib.utils.configfile import configFileParser
 from thirdparty.colorama.initialise import init as winowsColorInit
 
 sys.dont_write_bytecode = True  # 不生成pyc文件
 try:
-    __import__("lib.utils.versioncheck")  # this has to be the first non-standard import
+    # this has to be the first non-standard import
+    __import__("lib.utils.versioncheck")
 except ImportError:
     exit("[!] wrong installation detected (missing modules). Please install python version for 2.7.x")
-
 
 
 def modulePath():
@@ -45,6 +42,7 @@ def modulePath():
         _ = inspect.getsourcefile(modulePath)
 
     return getUnicode(os.path.dirname(os.path.realpath(_)), encoding=sys.getfilesystemencoding())
+
 
 def checkEnvironment():
     try:
@@ -68,25 +66,27 @@ def main():
     """
     Main function of w9scan when running from command line.
     """
-    checkEnvironment() # 检测环境
-    setPaths(modulePath()) # 为一些目录和文件设置了绝对路径
-    
+    checkEnvironment()  # 检测环境
+    setPaths(modulePath())  # 为一些目录和文件设置了绝对路径
+
     parser = argparse.ArgumentParser(description="w9scan scanner")
-    parser.add_argument("--update", help="update w9scan",action="store_true")
-    parser.add_argument("--guide", help="w9scan to guide",action="store_true")
-    parser.add_argument("--banner", help="output the banner",action="store_true")
+    parser.add_argument("--update", help="update w9scan", action="store_true")
+    parser.add_argument("--guide", help="w9scan to guide", action="store_true")
+    parser.add_argument(
+        "--banner", help="output the banner", action="store_true")
     parser.add_argument("-u", help="url")
-    parser.add_argument("-p","--plugin", help="plugins")
-    parser.add_argument("-s","--search",help="find infomation of plugin")
-    parser.add_argument("--debug",help="output debug info",action="store_true",default = False)
+    parser.add_argument("-p", "--plugin", help="plugins")
+    parser.add_argument("-s", "--search", help="find infomation of plugin")
+    parser.add_argument("--debug", help="output debug info",
+                        action="store_true", default=False)
     args = parser.parse_args()
 
     if IS_WIN:
         winowsColorInit()
     Banner()
-    
+
     try:
-        configFileParser(os.path.join(paths.w9scan_ROOT_PATH,"config.conf"))
+        configFileParser(os.path.join(paths.w9scan_ROOT_PATH, "config.conf"))
         initOption(args)
         pluginScan()
         webScan()
@@ -102,16 +102,18 @@ def main():
     except ToolkitUserQuitException:
         systemQuit(EXIT_STATUS.USER_QUIT)
 
-    except ToolkitPluginException,e:
+    except ToolkitPluginException, e:
         createIssueForBlog(e)
-        logger.warning('It seems like you reached a unhandled exception, We have automatically uploaded the exception information, please wait for a later update.')
+        logger.warning(
+            'It seems like you reached a unhandled exception, We have automatically uploaded the exception information, please wait for a later update.')
 
     except KeyboardInterrupt:
         systemQuit(EXIT_STATUS.USER_QUIT)
 
     except Exception as info:
-        logger.warning("error:%s "%(str(Exception) + " " + str(info)))
+        logger.warning("error:%s " % (str(Exception) + " " + str(info)))
         logger.warning('It seems like you reached a unhandled exception, please report it to author\'s mail:<master@hacking8.com> or raise a issue via:<https://github.com/boy-hacl/w9scan/issues/new>.')
+
 
 if __name__ == '__main__':
     main()
