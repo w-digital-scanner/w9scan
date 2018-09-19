83ba974a32195f12e74eb2bd84649866a791d4bf
boy-hack <34109680@qq.com>
2018-09-19 10:54:17 +0800

    typo for engine.py

--- lib/core/engine.py
+++ lib/core/engine.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 # project = https://github.com/boy-hack/w9scan
 # author = w8ay
-from lib.core.data import logger,urlconfig
-import time,sys
+from lib.core.data import logger, urlconfig
+import time
+import sys
 from lib.core.exploit import Exploit_run
 from lib.core.settings import LIST_PLUGINS
-from lib.core.common import printMessage
 from lib.utils import crawler
+
 
 def pluginScan():
     if not urlconfig.usePlugin:
@@ -20,13 +21,14 @@ def pluginScan():
     for u in urlconfig.url:
         logger.info('ScanStart Target:%s' % u)
         e.setCurrentUrl(u)
-        e.load_modules(urlconfig.plugin,u)
+        e.load_modules(urlconfig.plugin, u)
         e.run()
         time.sleep(0.01)
     endTime = time.clock()
     urlconfig.runningTime = endTime - startTime
     e.report()
     sys.exit()
+
 
 def webScan():
     startTime = time.clock()
@@ -35,7 +37,7 @@ def webScan():
     for url in urlconfig.url:
         logger.info('ScanStart Target:%s' % url)
         e.setCurrentUrl(url)
-        e.load_modules("www",url)
+        e.load_modules("www", url)
         e.run()
         if not urlconfig.mutiurl:
             e.init_spider()
